56eb5b8b04db153f7ada4321b7f54fd5dbc31829
yatharth2025 <yatharthyadavgolu@gmail.com>
2026-07-11 23:35:17 +0530

    refactor: modularize link and image scanners

---
+++ backend/src/scanners/console.scanner.ts
@@ -0,0 +1,23 @@
+import type { Page, ConsoleMessage } from "playwright";
+
+export const getConsoleMessages = async (page: Page) => {
+
+    const consoleMessages: {
+        type: string;
+        text: string;
+    }[] = [];
+
+    page.on("console", (message: ConsoleMessage) => {
+
+        consoleMessages.push({
+            type: message.type(),
+            text: message.text(),
+        });
+
+    });
+
+    return {
+        consoleMessages,
+    };
+
+};
