d2465d1b0cdfef179217686946ca877ab264b6fc
yatharth2025 <yatharthyadavgolu@gmail.com>
2026-07-19 00:05:14 +0530

    feat: create dashboard layout with sidebar and navbar

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div className="p-8">
+      <h2 className="text-3xl font-bold">
+        Welcome 👋
+      </h2>
+
+      <p className="text-slate-500 mt-2">
+        AI-powered website quality dashboard.
+      </p>
+    </div>
+  );
+}
+
+export default Dashboard;
