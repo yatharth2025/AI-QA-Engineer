324acbf51c0e367d81cd50ccc35eab7ed901aa27
yatharth2025 <yatharthyadavgolu@gmail.com>
2026-07-20 22:01:10 +0530

    feat: connect React dashboard with backend

---
+++ backend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+
+    baseURL: "http://localhost:5000",
+
+    headers: {
+
+        "Content-Type": "application/json",
+
+    },
+
+});
+
+export default api;
